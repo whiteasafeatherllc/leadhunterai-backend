958dcf10066dd165998882906cdebbcd24203aaf
whiteasafeatherllc <whiteasafeatherllc@gmail.com>
2025-08-17 02:37:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from scrapers.async_scrapers import fetch_leads
 
 app = FastAPI()
 
+# Allow your frontend domain
+origins = [
+    "http://leadhunterapp.superlativeorganics.shop",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -14,5 +19,7 @@ app.add_middleware(
 
 @app.get("/search")
 async def search(keyword: str):
+    if not keyword:
+        raise HTTPException(status_code=400, detail="Keyword is required")
     results = await fetch_leads(keyword)
     return results
